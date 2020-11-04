46fbee41422df82be14d6160b01b90e974a25648
Wene Alves <weneplay5@gmail.com>
2020-11-04 19:59:50 -0300

    adicionei as rotas de envios , as configuração de rotas

--- src/index.js
+++ src/index.js
@@ -12,10 +12,51 @@ const configs = {
     user: process.env.USER,
     localhost: process.env.HOST,
     password: process.env.PASSWORD,
+    host: process.env.HOST_NAME
 }
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 app.post('/email',(req, res) =>{
 
+    let emailTo = req.body.email
+    let emailID = req.body.id
+
+    const transport = nodemailer.createTransport({
+        host:configs.localhost,
+        port:configs.port[1],
+        secure:false,
+        auth:{user: configs.user
+            ,pass: configs.password
+        },
+        tls:{
+            rejectUnauthorized:true
+        }
+    })
+
+    transport.sendMail({
+        from: configs.user,
+        to: emailTo,
+        subject:'Sua Pergunta foi respondida!!',
+        text:`Acesse o site para ver a <a href="${configs.host}/pergunta/${emailID}">RESPOSTA!</a>`
+    }).then(info =>{
+        res.status(200)
+        res.json({
+            ok: 'enviado'
+        })
+    }).catch(err =>{
+        res.status(501)
+        res.json({
+            ok: 'não enviado'
+        })
+
+    })
+
+})
+
+app.listen(configs.port[0], _ =>{
+    console.log('Servidor de Envio de EMAIl rodando...... '+configs.port[0])
 })
 
 
